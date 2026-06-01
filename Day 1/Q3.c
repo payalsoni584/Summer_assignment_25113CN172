f5fc40c672496f8457ef0aeda7ea03f5c7e20fcd
payalsoni584 <payalsonimau@gmail.com>
2026-06-01 19:15:29 +0530

    Day 1 Uploaded

---
+++ Day 1/Q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n ,i;
+    printf("enter a number;");
+    scanf(%d",&n);
+        if(n<0){
+        printf("factorial is not defined for negative numbers\n");
+        else{
+        for(i=1;i<=n;i++){
+        factorial=factorial*1;
+        }
+        printf("factorial of %d=%11d",n,factorial);
+        }
+        return0;
+        }
